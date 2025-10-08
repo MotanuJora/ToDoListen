367c3ecca124661943815561a9635669c9b451d4
Tertiuc, Maximilian <Maximilian.Tertiuc@goldbeck.de>
2025-10-08 15:08:55 +0200

    Das Erstellen von Listen
    Das Erstellen von Listen wurde programmiert.
    
    Wenn das Programm läuft, fordert er dem Benutzer auch, eine neue Liste zu erstellen.
    
    Diese Liste bekommt ein Titel und einen ID.
    
    Diese Informationen werden in lists.json gespeichert. Alles was in lists.json früher gewesen ist, wird überschrieben. Das heißt wenn man das Programm wieder startet und eine neue Liste hinzufügt, wird die andere nicht gespeichert sondern gelöscht.

--- ToDoListen/Program.cs
+++ ToDoListen/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+using ToDoLists;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+        List<TodoList> allLists = new List<TodoList>();
+
+        Console.WriteLine("TodoList App V. 1.0");
+
+        Console.WriteLine("Currently, there are no To-do lists available");
+        Console.WriteLine("Please enter the name of your new list: ");
+
+        string userInput = Console.ReadLine();
+
+        TodoList newList = new TodoList(userInput);
+        allLists.Add(newList);
+
+        SaveLists(allLists, "lists.json");
+
+        Console.WriteLine(":");
+
+        Console.WriteLine($"New list created! ID: {newList.TodoListId}, Name: {newList.TodoListTitle}");
+        Console.WriteLine("You can now add tasks to your list");
+
+
+    }
+
+    static void SaveLists(List<TodoList> allLists, string lists)
+    {
+        string json = JsonSerializer.Serialize(allLists, new JsonSerializerOptions { WriteIndented = true });
+        System.IO.File.WriteAllText(lists, json);
+    }
+}
+     
--- ToDoListen/TodoList.cs
+++ ToDoListen/TodoList.cs
@@ -7,12 +7,33 @@ using System.Threading.Tasks;
 
 namespace ToDoLists
 {
+
     class TodoList
     {
-        private string TodoListTitle;
-        private string TodoListDescription;
-        public bool IsDone;
-        public int TodoListId;
+
+        public string TodoListTitle {  get; set; }
+        private string TodoListDescription {  get; set; }
+        public bool IsDone = false;
+        public int TodoListId { get;}
+
+
+
+        public List<string> Tasks { get; set; }
+
+        private static int _nextId = 1;
+        public TodoList(string todoListTitle) {
+
+            TodoListId = _nextId++;
+            TodoListTitle = todoListTitle;
+            Tasks = new List<string>();
+            Console.WriteLine($"{TodoListTitle} list created!");
+        
+        }
+        
+        
+        
+        
+
 
 
      
