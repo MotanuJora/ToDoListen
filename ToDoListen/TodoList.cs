ba44774e2138bd72aaf6285c763a7ff19279ad01
Tertiuc, Maximilian <Maximilian.Tertiuc@goldbeck.de>
2025-10-08 10:19:31 +0200

    Umbenennung des Projektes
    Projekt von "TodoListen" in "TodoLists" umbenannt.

--- ToDoListen/TodoList.cs
+++ ToDoListen/TodoList.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ToDoListen
+namespace ToDoLists
 {
-    internal class TodoList
+    class TodoList
     {
+        private string TodoListTitle;
+        private string TodoListDescription;
+        public bool IsDone;
+        public int TodoListId;
 
+
+     
     }
 }
